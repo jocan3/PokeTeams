6a775297763f0cb04becf9ce13481d5464f04891
jocan3 <andres.menaarias@gmail.com>
2020-03-15 14:07:30 -0600

    Adding summary column

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -16,7 +16,7 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class TeamsComponent implements OnInit, AfterViewInit {
 
   teams: Team[];
-  displayedColumns = ['team', 'usage_count', 'win_count', 'users_count', 'usage_ratio', 'win_ratio', 'ladder_rank','relevance','actions'];
+  displayedColumns = ['team', 'summary', 'usage_count', 'win_count', 'users_count', 'usage_ratio', 'win_ratio','relevance','actions'];
   dataSource = new MatTableDataSource<Team>();
   startDate : string;
   endDate: string;
